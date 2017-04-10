92a5bf7354024be090ea0a9087f0a645bc11c724
Fabio Kung <fabio.kung@gmail.com>
2017-04-10 09:54:29 -0700

    avoid saving container state to disk before daemon.Register
    Migrate legacy volumes (Daemon.verifyVolumesInfo) before containers are
    registered on the Daemon, so state on disk is not overwritten and legacy
    fields lost during registration.
    
    Signed-off-by: Fabio Kung <fabio.kung@gmail.com>

--- daemon/volumes_unix.go
+++ daemon/volumes_unix.go
@@ -169,7 +169,6 @@ func (daemon *Daemon) verifyVolumesInfo(container *container.Container) error {
 				container.MountPoints[destination] = &m
 			}
 		}
-		return container.CheckpointTo(daemon.containersReplica)
 	}
 	return nil
 }
